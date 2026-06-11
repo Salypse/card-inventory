fcabe8fb3e8b28218561ece6bb18b7a8ef207b73
Salypse <84153483+Salypse@users.noreply.github.com>
2026-06-11 13:24:18 -0500

    Update inventory table card_condition column name to condition

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 async function insertNewInventory(data) {
     await pool.query(
-        "INSERT INTO inventory (name, game, image, card_condition, is_foil, quantity) VALUES ($1, $2, $3, $4, $5, $6)", 
+        "INSERT INTO inventory (name, game, image, condition, is_foil, quantity) VALUES ($1, $2, $3, $4, $5, $6)", 
         [data.cardName, data.cardGame, data.image, data.condition, data.isFoil !== undefined ? true : false, data.quantity ])
 }
 
