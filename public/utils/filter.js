827adaab41dcd99dd801840617c04595a492f270
Brandon <84153483+Salypse@users.noreply.github.com>
2026-07-02 11:56:27 -0500

    Add responsive styling to filter component that changes orientation based on screen size

---
+++ public/utils/filter.js
@@ -0,0 +1,25 @@
+const toggleFilterOnBtn = document.getElementById("toggle-filter-on");
+const toggleFilterOffBtn = document.getElementById("toggle-filter-off");
+
+const filter = document.getElementById("filter");
+const nav = document.querySelector("nav");
+
+const inventory = document.getElementById("inventory-cards");
+const inventorySearch = document.getElementById("inventory-search");
+//Open or close filter on button press
+toggleFilterOnBtn.addEventListener("click", () => {
+  filter.style.top = `${nav.offsetHeight}px`;
+  filter.style.display = filter.style.display === "flex" ? "none" : "flex";
+
+  //Make background elements not focussable
+  inventory.inert = true;
+  inventorySearch.inert = true;
+});
+
+toggleFilterOffBtn.addEventListener("click", () => {
+  filter.style.display = filter.style.display === "flex" ? "none" : "flex";
+
+  //Reenable foccusable inventory elements
+  inventory.inert = false;
+  inventorySearch.inert = false;
+});
